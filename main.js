d969bd5ff13be225f965db35b88a0e698c25e0ee
Yuvraj-Thakrar <75614135+Yuvraj-Thakrar@users.noreply.github.com>
2021-06-16 11:03:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function go_on_football(){
+    window.location="football.html";
+}
+
+function go_on_desk(){
+    window.location="desk.html";
+}
+
+function go_on_bed(){
+    window.location="bed.html";
+}
+function go_on_fruit(){
+    window.location="fruit.html";
+}
